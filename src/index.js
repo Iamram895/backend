e3012f953820161f47bd922454d5cde095a33425
shriram <choudharyshriram53@gmail.com>
2023-12-16 14:42:21 +0530

    uitls code  make

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 //require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db connection failed!!", error);
+  });
 
+// Method Second //
 /*
 import express from "express";
 const app = express();
